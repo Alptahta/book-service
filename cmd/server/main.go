3a903d9a0f5970822dc48f8648247a59b1123afc
Alptahta <alp.tahta@protonmail.com>
2024-04-15 15:24:03 +0300

    Init HTTP server with health endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+const HTTP_SERVER_PORT = 8080
 
 func main() {
-	fmt.Println("init")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
+
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", HTTP_SERVER_PORT),
+		Handler: mux,
+	}
+
+	log.Printf("Starting HTTP server at port %d", HTTP_SERVER_PORT)
+	log.Fatal(srv.ListenAndServe())
 }
